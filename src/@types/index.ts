d0aafcc837c6315e85ee341a30373658b9fe02f9
Al Nayeem <nayeem2019@hotmail.com>
2025-11-19 15:01:59 +0000

    completed mathch list libaray page

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -3,3 +3,9 @@ import { User } from 'firebase/auth'
 export interface AuthContextType {
   user: User | null | undefined // undefined = checking, null = not logged in
 }
+
+export interface Match {
+  id: number;
+  status: 'PENDING' | 'PROCESSING' | 'COMPLETED';
+  cratedAt: string;
+}
